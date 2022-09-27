e264277efde9cd40e042d3735110bb6a8022fe38
johnny lee <johnnylee@turtle-v4.local>
2022-09-26 18:43:09 -0700

    started lab 3 bme490

---
+++ lab3_finger_joint_mvmt.m
@@ -0,0 +1,19 @@
+clc
+
+x1 = DIPx;
+y1 = DIPy;
+
+x2 = PIPx;
+y2 = PIPy;
+
+x3 = MCPx;
+y3 = MCPy;
+
+k = 0;
+
+for x = 1:1:802 %each x,y position of each joint has 802 points of data
+    k = k + 1;
+
+    ds1(k) = (x1)
+
+end
