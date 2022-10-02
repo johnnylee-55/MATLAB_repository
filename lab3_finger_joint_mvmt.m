1fc352daff1bda2aa29e19970de5924532c1a6f7
johnny lee <johnnylee@turtle-v4.local>
2022-10-01 20:43:05 -0700

    added to lab3 .m file

--- lab3_finger_joint_mvmt.m
+++ lab3_finger_joint_mvmt.m
@@ -65,8 +65,40 @@ grid on;
 % import voltage data from strain guage sensor
 t2 = timeSensor;
 V = voltageSensor;
+% plot voltage vs time from strain guage
 figure
 plot(t2,V)
 title('Voltage over time from Strain Guage');
 xlabel('time (s)');
 ylabel('Voltage (V)')
+
+% takes set of points (x,y) and outputs the coefficients found by the
+% quadratic regression (a0, a1, a2) where y(x)= a2*x^2 + a1*x + a0
+function a = QuadFit(x,y)
+
+% number of loops
+n = length(V);
+
+% find the sums needed for building two equations for solving least squares
+% best-fit method
+s_x1=0; s_x2=0; s_x3=0; s_x4=0;
+s_y=0; s_xy=0; s_x2y=0;
+
+for i = 1:n
+    s_x1 = s_x1 + (x(i));
+    s_x2 = s_x2 + (x(i)).^2;
+    s_x3 = s_x3 + (x(i)).^3;
+    s_x4 = s_x4 + (x(i)).^4;
+
+    s_y = s_y + y(i);
+    s_xy = s_xy + x(i)*y(i);
+    s_x2y = s_x2y + x(i).^2*y(i);
+end
+
+% matrix set up
+A = [ n s_x1 s_x2 ; s_x s_x2 s_x3 ; s_x2 s_3 s_x4 ];
+b = [ s_y ; s_xy ; s_x2y ];
+
+c = gauss(A,b);
+a = [ c(3) c(2) c(1) ]
+end
