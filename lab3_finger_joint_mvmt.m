09951db98f433a9babfeca52ffd76251e0d300d5
johnny lee <johnnylee@turtle-v4.local>
2022-10-06 14:40:49 -0700

    cleaned up lab3_workspace, corrected for hardcoded matrix sizes

--- lab3_finger_joint_mvmt.m
+++ lab3_finger_joint_mvmt.m
@@ -12,8 +12,9 @@ y2 = PIPy;
 x3 = MCPx;
 y3 = MCPy;
 
+% loop calculates angle of PIP joint over time
 k = 0;
-for x = 1:1:802 %each x,y position of each joint has 802 points of data
+for x = 1:1:size(PIPx) % each x,y position of each joint has 802 points of data
     k = k + 1;
 
     % distance between each joint
@@ -22,16 +23,16 @@ for x = 1:1:802 %each x,y position of each joint has 802 points of data
     Dist31(k) = (x3(k) - x1(k))^2 + (y3(k) - y1(k))^2;
 
     % calculate side length of each triangle
-    Side12(k) = sqrt(D12(k));
-    Side23(k) = sqrt(D23(k));
+    Side12(k) = sqrt(Dist12(k));
+    Side23(k) = sqrt(Dist23(k));
     Side31(k) = sqrt(Dist31(k));
 
     % calculate PIP joint angles from distances and side lengths
-    PIPangle(k) = acosd((Dist31(k) - D23(k) - D12(k)) / (-2*Side12(k)*Side23(k)));
+    PIPangle(k) = acosd((Dist31(k) - Dist23(k) - Dist12(k)) / (-2*Side12(k)*Side23(k)));
 end
 
 % plot X,Y positions of joints over time
-subplot (2,2,1)
+subplot(2,2,1)
 plot(x1, y1, 'or');
 title('Position of DIP Joint over time');
 xlabel('X Position of DIP Joint (cm)');
@@ -52,7 +53,7 @@ xlabel('X Position of MCP Joint (cm)');
 ylabel('Y Position of MCP Joint (cm)');
 
 % plot PIP joint angle over time
-k = 1:1:802; % length of data is 802
+k = 1:1:size(PIPx);
 
 subplot(2, 2, 4)
 plot(t1(k), PIPangle(k), '--');
@@ -72,14 +73,13 @@ title('Voltage over time from Strain Guage');
 xlabel('Time (s)');
 ylabel('Voltage (V)')
 
-% resample data sets to have 800 data points
+% priot to using QuadFit(), resample both data sets to have 800 data points
 voltageRS = resample(voltageSensor, 800, 799);
 angleRS = resample(PIPangle, 800, 802);
 
-% prior to using QuadFit, data must be resampled to have the same length
-resultCoefficients = QuadFit(voltageRS, angleRS)
+resultCoefficients = QuadFit(voltageRS, angleRS);
 
-% plot resampled voltage/PIPangle with quadratic fit equation
+% plot resampled voltage/PIPangle with quadratic fit equation on top
 figure
 plot(voltageRS, angleRS,'b--o')
 hold on
@@ -87,14 +87,16 @@ a1 = resultCoefficients(1);
 a2 = resultCoefficients(2);
 a3 = resultCoefficients(3);
 x = 0.75:.1:2.5;
-y = (a1*x.^2 + a2*x + a3);
+y = (a1*x.^2 + a2*x + a3); % uses coefficients calculated by QuadFit() function
 plot(x,y,'r','LineWidth', 2.0)
 title('Voltage vs PIPjoint Angle with Quadratic Line Fit')
 xlabel('Voltage (V)')
 ylabel('PIP Joint Angle Measurement (degrees)')
 
-% takes set of points (x,y) and outputs the coefficients found by the
-% quadratic regression (a0, a1, a2) where y(x)= a2*x^2 + a1*x + a0
+%{
+ takes set of points (x,y) and outputs the coefficients found by the
+ quadratic regression (a0, a1, a2) where y(x)= a2*x^2 + a1*x + a0
+%}
 function a = QuadFit(x,y)
 
 % number of loops
@@ -116,7 +118,7 @@ for i = 1:n
     s_x2y = s_x2y + x(i).^2*y(i);
 end
 
-% matrix set up
+% build matrix for Gauss Elimination method
 A = [ n s_x1 s_x2 ; s_x1 s_x2 s_x3 ; s_x2 s_x3 s_x4 ];
 b = [ s_y ; s_xy ; s_x2y ];
 
@@ -124,7 +126,7 @@ c = gauss(A,b);
 a = [ c(3) c(2) c(1) ];
 end
 
-% matrix calculation
+% matrix calculation using Guass Elimination
 function x = gauss(A,b)
 
 n = length(b);
