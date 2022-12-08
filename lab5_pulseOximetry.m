f3f14a81e9c29ebcd7d4d5a1c462b93daf5522ae
Johnny Lee <leejohnny5533@gmail.com>
2022-12-07 21:04:38 -0800

    lab5 finished SpO2 calculation

--- lab5_pulseOximetry.m
+++ lab5_pulseOximetry.m
@@ -23,8 +23,10 @@ heartRateValues = (60./e)*128;
 
 
 %% SpO2
-t = 1:1:552;
-t2 = 1/128;
+t = 1:1:553;
+t2 = t/128;
+t2 = t2';
+
 % R-value constants
 a = -16.666667;
 b = 8.333333;
@@ -32,10 +34,10 @@ c = 100;
 % peak detection of PPG
 red = lowpass(red, 20, fs);
 redSegment = red(2300:2852);
-infraredSegment = flatSegment;
+infraredSegment = infrared(2300:2852);
 
 DCred = rms(redSegment);
-DCinfrared = rms(flatSegment);
+DCinfrared = rms(infraredSegment);
 
 
 index = 1;
@@ -45,13 +47,24 @@ for i = 1:ppgPeaks-1
 end
 
 % peak detection of red light
-[ACredSignal, ACredPosition] = detectACPeaks(redSegment, DCred);
+[redPeakVal, ACredPosition, AcredSignal] = detectACPeaks(redSegment, DCred);
 
 % peak detection of infrared light
-[ACinfraSignal, ACinfraPosition] = detectACPeaks(infraredSegment, DCinfrared);
+DCinfrared = DCinfrared + 1000; % +1000 for better peak detection
+[infraPeakVal, ACinfraPosition, ACinfraSignal] = detectACPeaks(infraredSegment, DCinfrared);
 
+% SpO2 calculation
+for i = 1 : index
+    R(i) = ((redPeakVal(i)-ACredSignal(i))/DCred)/((infraPeakVal(i)-ACinfraSignal(i))/DCinfrared);
+    SpO2(i) = a*R(i)*R(i) + (b*R(i)) + c;
+end
 
-
+% plot functions
+yyaxis left
+plot(t2, redSegment); hold on; plot(t2, infraredSegment); hold on;
+yyaxis right
+plot(ACredPosition/128, SpO2);
+axis([0 5 80 110])
 
 %% Function Definitions
 function [peakVal, peakPos, peakInd] = detectPeaks(array, threshold)
@@ -66,17 +79,15 @@ function [peakVal, peakPos, peakInd] = detectPeaks(array, threshold)
     end
 end
 
-
-function [ACsignal, peakPos] = detectACPeaks(array, DC)
+function [peakVal, peakPos, ACsignal] = detectACPeaks(array, DC)
     index = 1;
     n = length(array);
     for i=2:n-1
-        if(array(i)>array(i-1) && array(i)>=array(i+1) && array(i) > DC)
+        if array(i)>array(i-1) && array(i)>=array(i+1) && array(i) > DC
             peakVal(index) = array(i);
             peakPos(index) = i;
-            ACsignal(index) = array(peakPos(index));
+            ACsignal(index) = array(peakPos(index)-25);
             index = index + 1;
         end
     end
 end
-
