f695aee2e4c328406175e2b6a7495565d4c292f6
Johnny Lee <leejohnny5533@gmail.com>
2022-12-09 21:46:20 -0800

    added heart rate animation

--- lab2_human_gait_analysiss.m
+++ lab2_human_gait_analysiss.m
@@ -57,8 +57,8 @@ for i = 1:243 % data length
     plot([A1(i,1) B1(i,1)], [A2(i,1) B2(i,1)], 'r', ...
          [B1(i,1) C1(i,1)], [B2(i,1) C2(i,1)], 'g', ...
          [C1(i,1) D1(i,1)], [C2(i,1) D2(i,1)], 'b');
-        axis([0 2.75 0 1.4]); % axis range
-   title("Motion of Right Leg for one Stride");
+    axis([0 2.75 0 1.4]); % axis range
+    title("Motion of Right Leg for one Stride");
         hold on;
         drawnow
 end
--- test.m
+++ test.m
@@ -1,2 +1,8 @@
-plot(redSegment);
 
+set(groot, 'defaultLineLineWidth', 1)
+for i = 1 : 1151
+    plot(i,redSegment);
+    %axis([0 10 350000 370000]);
+    hold on;
+    drawnow
+end
